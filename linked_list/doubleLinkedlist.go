af9e76289e05256e3a67f447d3413a19fb03c6a7
BALAJI V <92379046+BaLa071@users.noreply.github.com>
2023-08-08 15:20:07 +0530

    Update doubleLinkedlist.go

--- linked_list/doubleLinkedlist.go
+++ linked_list/doubleLinkedlist.go
@@ -44,6 +44,36 @@ func insertAtMiddle(val int, position int, head *node) {
 	}
 }
 
+func deletion(val int, head, root **node) {
+	if (*head).data == val {
+		temp := (*head).next
+		(*head).next = nil
+		temp.prev = nil
+		*head = temp
+	} else if (*root).data == val {
+		temp := (*root).prev
+		(*root).prev = nil
+		temp.next = nil
+		*root = temp
+	} else {
+		temp := *head
+		for temp.data != val {
+			temp = temp.next
+		}
+		flag := temp.prev
+		flag1 := temp.next
+		temp.prev.next, temp.next.prev = flag1, flag
+	}
+}
+
+func display(head *node) {
+	for head != nil {
+		fmt.Print(head.data, " ")
+		head = head.next
+	}
+	fmt.Println()
+}
+
 func main() {
 	var head, tail *node
 	head = createNode(1)
@@ -54,12 +84,15 @@ func main() {
 	insertAtLast(2, &tail)
 	insertAtLast(3, &tail)
 	insertAtLast(5, &tail)
-
 	insertAtMiddle(4, 3, head)
 
 	//printing statement
-	for head.next != nil {
-		fmt.Println(head.data)
-		head = head.next
-	}
+	display(head)
+
+	deletion(3, &head, &tail)
+	deletion(0, &head, &tail)
+	deletion(5, &head, &tail)
+
+	//printing list after deletion
+	display(head)
 }
